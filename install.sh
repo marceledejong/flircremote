8fbb7dda21044d3de20663925bacf2048313f61f
marceledejong <marceledejong@gmail.com>
2016-11-27 12:58:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 echo "Installing Flirc remote dependencies"
-sudo apt-get update
-sudo apt-get -y install brutefir
-echo "Installing brutefir plugin"
+npm install socket.io
 
-echo "snd_aloop" > /etc/modules
+
 
 #requred to end the plugin install
 echo "plugininstallend"
